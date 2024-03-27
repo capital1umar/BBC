24b1df18af194c6a194bec95a5d075b0fc855036
capital1umar <musaumarfaruq17@gmail.com>
2024-03-27 13:02:06 -0400

    updated

---
+++ script.js
@@ -0,0 +1,25 @@
+const mobileNav = document.getElementById("mobileNav");
+const openNav = document.querySelector("#openNav");
+
+// function openNavigation() {
+//   const openNav = document.querySelector("#openNav");
+//   openNav.style.display = "flex";
+//   mobileNav.style.display = "none";
+// }
+
+function openNavigation() {
+  const openNav = document.querySelector("#openNav");
+  const mobileNav = document.querySelector("#mobileNav");
+
+  openNav.style.display = "flex";
+  openNav.style.opacity = "1";
+  openNav.style.pointerEvents = "auto";
+  openNav.style.visibility = "visible";
+
+  mobileNav.style.display = "none";
+}
+
+function closeNavigation() {
+  openNav.style.display = "none";
+  mobileNav.style.display = "block";
+}
